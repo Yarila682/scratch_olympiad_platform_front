22f2852bc6f3e79c25d9ffa58c60bd8d18a0b4d9
Yarila682 <yarila682@yandex.ru>
2025-03-07 05:24:38 +0000

    Set up eslint config.

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import reactRefresh from "eslint-plugin-react-refresh";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+)), {
+    plugins: {
+        "react-refresh": reactRefresh,
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        "react-refresh/only-export-components": "warn",
+        "@typescript-eslint/no-unused-vars": "error",
+    },
+    ignores: ["./src/__generated__/"]
+}];
